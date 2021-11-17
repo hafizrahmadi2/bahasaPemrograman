96c2ea3e60c758223bf82bdb31a622c65c64df8f
root <root@LAPTOP-TVHMV7T4.localdomain>
2021-11-17 19:10:19 +0700

    fresh from oven

---
+++ uts/soal4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int tahunlahir = 2000;
+    int tahunsekarang = 2021;
+    int usia;
+
+    usia = tahunsekarang - tahunlahir;
+    
+    cout<<"Berarti Umur kamu sekarang = "<<usia<<"Tahun";
+
+    return 0;
+}
